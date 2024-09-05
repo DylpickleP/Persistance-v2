072eca475566cc673391ee452f6111e1bc5fc77a
Dylan Borgio <dylanborgio24@gmail.com>
2024-09-05 08:52:29 -0500

    Initial Commit

---
+++ Persistance v2/Persistance_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Persistance_v2App.swift
+//  Persistance v2
+//
+//  Created by Dylan Borgio on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Persistance_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
